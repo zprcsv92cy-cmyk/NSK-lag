281220da6bad581c1bbf5d1fe78b9fd5f344bb31
zprcsv92cy-cmyk <zprcsv92cy@privaterelay.appleid.com>
2026-03-07 11:50:00 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// NSK Team 18 Service Worker (v1.9.50)
-const CACHE_NAME = "nsk-team18-v1.9.50";
+// NSK Team 18 Service Worker (v1.9.51)
+const CACHE_NAME = "nsk-team18-v1.9.51";
 const ASSETS = [
   "./",
   "./index.html",
